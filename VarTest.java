78a63f1e2f42dc0996f5f0998d8696518d6003f1
hxy2006 <79700623+hxy2006@users.noreply.github.com>
2024-04-01 14:26:00 +0800

    Add files via upload

---
+++ VarTest.java
@@ -0,0 +1,19 @@
+public class VarTest{
+    public static void main(String[] args){
+        //声明一个int变量起名为age
+        int age;
+
+        //给age赋值
+        age = 20;
+
+        //读取数据
+        System.out.println("年龄=" + age);
+
+        //修改变量值
+        age = 30;
+
+        //再次读取数据
+        System.out.println("年龄=" + age);
+
+    }
+}
---
+++ VarTest01.java
@@ -0,0 +1,18 @@
+public class VarTest01 {
+    public static void main(String[] args) {
+        //定义一个变量，来定义Π
+        //变量的声明和赋值可以同时进行
+        double Π = 3.14;
+
+        System.out.println("圆周率：" + Π);
+
+        String name = "lucy";
+
+        System.out.println("name = " + name);
+
+        name = "jack";
+
+        System.out.println("name = " + name);
+
+    }
+}
